b3d222b96ebbebe05dd50f43486c2562a13ef412
marwa AlShahal <marwa.al.shahal@gmail.com>
2021-03-31 19:10:31 +0300

    feat: add navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,35 +1,76 @@
 import React from 'react';
-import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
-import {faBars} from "@fortawesome/free-solid-svg-icons";
+import {FaBars} from "react-icons/fa";
+import {Link } from 'react-scroll';
+//import {HashLink as Link} from "react-router-hash-link";
+import {animateScroll as scroll} from 'react-scroll'
 
 const Navbar = () => {
+  const toggleHome=()=>{
+    scroll.scrollToTop();
+  }
+
     return (
-<nav className="navbar navbar-expand-lg navbar-light bg-dark">
+<nav id="navbar"  className="navbar navbar-expand-lg navbar-light " style={{backgroundColor:"#5A0000"}}>
  <div className="container">
-  <a className="navbar-brand" href="#"><img className="logo" src="https://www.graphicsprings.com/filestorage/stencils/4912a084e18f1a5e09ab336e3ce156ee.png?width=500&height=500" alt="logo" ></img></a>
+  {/* <Link className="navbar-brand" to="/" onClick={toggleHome}>
+    <img className="logo" src="https://banner2.cleanpng.com/20180504/pew/kisspng-koszalin-logo-rose-logo-5aebf8f1180c42.9590656715254141290985.jpg" alt="logo" ></img>
+    </Link> */}
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <FontAwesomeIcon icon={faBars} style={{color:"#fff"}} />
+  
+    <FaBars style={{color:"#fff"}} />
   </button>
 
   <div className="collapse navbar-collapse" id="navbarSupportedContent">
     <ul className="navbar-nav ml-auto">
-      <li className="nav-item active">
-        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">about me</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">how work</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">portfolio</a>
-      </li>
-      <li className="nav-item">
-        <a className="nav-link" href="#">contacts</a>
-      </li>
 
-     
+      <li className="nav-item active">
+        <Link  className="nav-link" to="/" onClick={toggleHome}>Home <span className="sr-only">(current)</span></Link >
+      </li>
+      
+      <li className="nav-item">
+        <Link  className="nav-link" 
+           to="about"
+           smooth={true}
+           durartion={500}
+           spy={true}
+           exact='true' offset={-50}
+        >
+          about me
+          </Link >
+      </li>
+      <li className="nav-item">
+        <Link  className="nav-link" 
+           to="technologies"
+           smooth={true}
+           durartion={500}
+           spy={true}
+           exact='true' offset={-40}
+        >
+          skills
+          </Link >
+      </li>
+      <li className="nav-item">
+        <Link className="nav-link" 
+           to="projects"
+           smooth={true}
+           durartion={500}
+           spy={true}
+           exact='true' offset={-50}
+           >
+             projects
+             </Link >
+      </li>
+      <li className="nav-item">
+        <Link  className="nav-link"
+           to="contact"
+           smooth={true}
+           durartion={500}
+           spy={true}
+           exact='true' offset={-50}
+            >
+              contacts
+        </Link >
+      </li>
     </ul>
   </div>
   </div>
